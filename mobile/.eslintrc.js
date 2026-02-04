fcffccb6a4059f1e6b4e732bb370691ba5f268a2
PiotrO9 <piotr.ostrowski02@wp.pl>
2026-02-04 22:08:11 +0100

    chore: setup eslint and prettier with import sorting

---
+++ mobile/.eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  root: true,
+  ignorePatterns: ['.eslintrc.js', '.prettierrc.js'],
+  extends: [
+    'expo',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react-native/all',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'react-native', 'prettier'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'react/display-name': 'off',
+    'react-native/no-inline-styles': 'warn',
+    'react-native/no-color-literals': 'off',
+    'prettier/prettier': 'warn',
+    'no-console': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  env: {
+    'react-native/react-native': true,
+  },
+};
